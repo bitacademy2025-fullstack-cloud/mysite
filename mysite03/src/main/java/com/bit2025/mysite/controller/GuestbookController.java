abf170216572c7fd54a228cb017d9fdccd3d541f
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-09-02 17:21:39 +0900

    Update GuestbookController.java

--- mysite03/src/main/java/com/bit2025/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/bit2025/mysite/controller/GuestbookController.java
@@ -9,6 +9,29 @@ import com.bit2025.mysite.service.GuestbookService;
 public class GuestbookController {
 	@Autowired
 	private GuestbookService guetbookService;
+	@RequestMapping("")
+	public String index(Model model) {
+		model.addAttribute("list", guestbookService.getContentsList());
+		return "guestbook/index";
+	}
 	
+	@RequestMapping("/add")
+	public String add(GuestbookVo guestbookVo) {
+		guestbookService.addContents(guestbookVo);
+		return "redirect:/guestbook";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String delete(@PathVariable("id") Long id) {
+		return "/guestbook/delete";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.POST)
+	public String delete(
+		@PathVariable("id") Long id,
+		@RequestParam(value="password", required=true, defaultValue="") String password) {
+		guestbookService.deleteMessage(id, password);
+		return "redirect:/guestbook";
+	}
 	
 }
