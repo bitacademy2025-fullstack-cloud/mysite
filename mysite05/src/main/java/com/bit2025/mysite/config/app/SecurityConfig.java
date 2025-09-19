b630e0a948d2ce3ac342c3d899dfff2a5f2e34f1
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-09-19 17:32:49 +0900

    mysite05 - ss #3

--- mysite05/src/main/java/com/bit2025/mysite/config/app/SecurityConfig.java
+++ mysite05/src/main/java/com/bit2025/mysite/config/app/SecurityConfig.java
@@ -2,12 +2,21 @@ package com.bit2025.mysite.config.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+import com.bit2025.mysite.repository.UserRepository;
+import com.bit2025.mysite.security.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+        .csrf(csrf -> csrf.disable())
         .formLogin(formLogin -> {
         	formLogin
         		.loginPage("/user/login")
@@ -40,5 +50,24 @@ public class SecurityConfig {
     	});
         
         return http.build();
-    }    
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+    	DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    	authenticationProvider.setUserDetailsService(userDetailsService);
+    	authenticationProvider.setPasswordEncoder(passwordEncoder);
+    	
+    	return new ProviderManager(authenticationProvider);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder(4); // 4 ~ 31
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+    	return new UserDetailsServiceImpl(userRepository);
+    }
 }
